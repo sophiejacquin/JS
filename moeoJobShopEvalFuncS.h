3c080d560680ef02626ee580cb6732ef491b05b2
sophie <toe.jacquin@gmail.com>
2016-05-24 17:26:03 +0200

    fonction d'évaluation

---
+++ moeoJobShopEvalFuncS.h
@@ -0,0 +1,71 @@
+#ifndef _eoJSEvalFunc_h
+#define _eoJSEvalFunc_h
+
+// include whatever general include you need
+#include <stdexcept>
+#include <fstream>
+
+// include the base definition of eoEvalFunc
+#include "eoEvalFunc.h"
+
+/**
+  Always write a comment in this format before class definition
+  if you want the class to be documented by Doxygen
+*/
+template <class EOT>
+class moeoJSEvalFunc : public eoEvalFunc<EOT>
+{
+ public:
+  moeoJSEvalFunc(Data & data_)
+  {
+      data=data_;
+  }
+
+  /** Actually compute the fitness
+   *
+   * @param EOT & _eo the EO object to evaluate
+   *                  it should stay templatized to be usable 
+   *                  with any fitness type
+   */
+  void operator()(EOT & _eo)
+  {
+    // test for invalid to avoid recomputing fitness of unmodified individuals
+    if (_eo.invalid())
+      {
+         int tardiness =0;
+	 int earliness=0;
+          for(int i=0;i<data.getN(); i++)
+          {
+             
+                  int job=_eo.getJob(i);
+                  int e=0; int t=0;
+                  int c=_eo.getCompletionTime(i);
+                  int d=data.getJob(job).getD();
+                  int alpha=data.getJob(job).getAlpha();
+                  int beta =data.getJob(job).getBeta();
+                  //cout<<alpha<<" "<<beta<<" c "<<c<<" "<<_eo.getEval(i)<<" d "<<d<<" fit "<<fit<<endl;
+                  if(c<d)
+                  {
+                      e=alpha*(d-c);
+                  }
+                  else if(c>d)
+                  {
+                      t=beta*(c-d);
+                  }
+                  earliness+=e;
+	          tardiness+=t;
+
+              
+          }
+	moeoUCPObjectiveVector objVec;
+	objVec[0]=earliness;
+	objVec[1]=tardiness;
+	_eo.objectiveVector(objVec);
+      }
+  }
+
+ private:
+    Data data;
+
+};
+#endif
