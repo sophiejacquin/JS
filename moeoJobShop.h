e1cb73857d97c5a80e68a7c7e47cdc14de9d5fc3
sophie <toe.jacquin@gmail.com>
2016-05-24 16:05:07 +0200

    definition classe représentation indirecte

--- moeoJobShop.h
+++ moeoJobShop.h
@@ -1,2 +1,55 @@
-class moeoJobShop{
-}
+#ifndef _eoJobShop_h
+#define _eoJobShop_h
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <addToParadisEO/moeo/core/Origin.h>
+class moeoJobShop : public moeoRealVector < moeoJobShopObjectiveVector >, public Origin{
+ public :
+	moeoJobShop()
+	{
+		N=0;
+	}
+	 virtual ~moeoJobShop()
+  	{
+  	}
+	virtual string className() const { return "eoJobShop"; }
+
+  
+    	void printOn(ostream& os) const
+    	{
+      
+     	 moeoIntVector<moeoJobShopObjectiveVector>::printOn(os);
+      	os << ' ';
+    
+    	}
+
+  
+    	void readFrom(istream& is)
+      	{
+	
+	moeoIntVector<eoJobShopObjectiveVector>::readFrom(is);
+    
+      	}
+	int operator [](i)
+	{
+		return listeJobs[i];
+	}
+	void setN(int N_)
+    	{
+        	N=N_;
+    	}
+    	int getN() const
+    	{
+       	 	return N;
+    	}
+	void setListeJobs(std::vector<int> liste)
+	{
+		listeJobs=liste;
+	}
+ private :
+ 	std::vector<int> listeJobs;
+	int N;
+};
+#endif
